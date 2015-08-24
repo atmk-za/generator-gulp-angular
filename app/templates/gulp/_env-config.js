8efbeb05c0614e28f04471e86d0b624354485f92
Neil R <atmkza@gmail.com>
2015-08-24 11:10:12 +0200

    default, dev, production

--- app/templates/gulp/_env-config.js
+++ app/templates/gulp/_env-config.js
@@ -5,13 +5,11 @@ var fs = require('fs');
 var argv = require('yargs').argv;
 var $ = require('gulp-load-plugins')();
 
-//var ngConstant = require('gulp-ng-constant');
 
 var environment = argv.env || 'default';
-//var ENV = JSON.parse(fs.readFileSync('./src/app/env_configs/config-' + environment + '.json', 'utf8')).ENV;
 
 gulp.task('env-config', function () {
-  return gulp.src('src/app/env_configs/config-' + environment + '.json')
+  return gulp.src('src/app/env-configs/config-' + environment + '.json')
     .pipe($.ngConstant({
       name: '<%- appName %>',
       deps: false,
